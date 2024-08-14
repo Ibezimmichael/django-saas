6ae2dd3d8b228e917a5b56105bc66e88af69fa25
Ibezimmichael <ibezim.michael@yahoo.com>
2024-08-14 06:04:56 +0100

    start for saas

---
+++ src/web/views.py
@@ -0,0 +1,18 @@
+import pathlib
+from django.http import HttpResponse
+from django.shortcuts import render
+from visits.models import PageVisit
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+
+def home_page_view(request, *args, **kwargs):
+    queryset = PageVisit.objects.filter(path=request.path)
+    my_title = "Michael"
+    my_context = {
+        "page_title": my_title,
+        "visits": queryset   
+    }
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, my_context)
